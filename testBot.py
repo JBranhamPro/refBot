92547f48d93b0521c1a85b134f85613f4a89b9b0
JBranhamPro <joshuajbranham@gmail.com>
2017-08-31 13:05:34 -0400

    Updated commands to reflect overhaul of Refbot
    /rollCall || changed to accommodate different data sets of players
    /on || added to test the same command in Refbot

--- testBot.py
+++ testBot.py
@@ -3,35 +3,38 @@ from discord.ext import commands
 import logging
 logging.basicConfig(level=logging.INFO)
 
-botToken = ''
+botToken = 'INSERT BOT TOKEN HERE'
 testBot = commands.Bot(command_prefix="/")
 
 @testBot.command()
-async def aDraft():
-	await testBot.say('!place TheRealN3lo')
-	await testBot.say('!place Ickyrus')
-	await testBot.say('!place Bobmicbiong')
-	await testBot.say('!place BrokenLegFish')
-	await testBot.say('!place TheUnsungHeroPt2')
-	await testBot.say('!place llamamalicious')
-	await testBot.say('!place ParadoxCycle')
-	await testBot.say('!place Zaraedaria')
-	await testBot.say('!place TheBigSpence')
-	await testBot.say('!place Xyeles')
-
-@testBot.command()
-async def rollCall():
-	await testBot.say('!aye TheRealN3lo')
-	await testBot.say('!aye Ickyrus')
-	await testBot.say('!aye Bobmicbiong')
-	await testBot.say('!aye BrokenLegFish')
-	await testBot.say('!aye TheUnsungHeroPt2')
-	await testBot.say('!aye llamamalicious')
-	await testBot.say('!aye ParadoxCycle')
-	await testBot.say('!aye Zaraedaria')
-	await testBot.say('!aye TheBigSpence')
-	await testBot.say('!aye Xyeles')
+async def rollCall(subset = 'a'):
+	if subset == 'b':
+		await testBot.say('!aye TheRealN3lo')
+		await testBot.say('!aye ECAEN')
+		await testBot.say('!aye HumbleDiligent')
+		await testBot.say('!aye FlameFlameFlame')
+		await testBot.say('!aye Xyeles')
+		await testBot.say('!aye BrokenLegFish')
+		await testBot.say('!aye TheBigSpence')
+		await testBot.say('!aye Bobmicbiong')
+		await testBot.say('!aye AnnieBot')
+		await testBot.say('!aye TheUnsungHeroPt2')
+	else:
+		await testBot.say('!aye TheRealN3lo')
+		await testBot.say('!aye Ickyrus')
+		await testBot.say('!aye Bobmicbiong')
+		await testBot.say('!aye BrokenLegFish')
+		await testBot.say('!aye TheUnsungHeroPt2')
+		await testBot.say('!aye llamamalicious')
+		await testBot.say('!aye ParadoxCycle')
+		await testBot.say('!aye Zaraedaria')
+		await testBot.say('!aye TheBigSpence')
+		await testBot.say('!aye Xyeles')
 
 	await testBot.say('!rollCall')
 
-testBot.run(botToken)
+@testBot.command()
+async def on():
+	await testBot.say('!on matchmade')
+	await testBot.say('!on rChamps')
+	await testBot.say('!on rLanes')
