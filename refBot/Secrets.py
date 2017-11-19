30adc459e674797cd53c30911656fdc28082b8bf
Josh Branham <joshuajbranham@gmail.com>
2017-11-19 18:08:43 -0500

    Added only to pull from remote repo

--- refBot/Secrets.py
+++ refBot/Secrets.py
@@ -1,2 +1,2 @@
-apiKey = 'RGAPI-53a7a791-6809-4db8-aafa-7a59c8f124d4'
-botToken = 'NrQsVVYHJK9h-wLrObraVd-Ztzxa-G7L'
+apiKey = 'RGAPI-61d1cbd1-5cd0-4fb4-b2e6-b21eda1cc174'
+botToken = 'MzMxOTMyNDQ3Mjk1OTMwMzY5.DPOAyw.T09jOCAB4iZFa7IbLO-pYsUwPs4'
